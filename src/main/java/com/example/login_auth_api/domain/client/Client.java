d6faa9fbbb1bcb2022c0fbc2c12f1402c81597e2
Roger Yuri de Araújo Lima <roger.lima@livsaude.com.br>
2024-07-22 10:57:41 -0300

    changing names of attributes

--- src/main/java/com/example/login_auth_api/domain/client/Client.java
+++ src/main/java/com/example/login_auth_api/domain/client/Client.java
@@ -8,16 +8,16 @@ import lombok.Setter;
 
 @Entity(name="clients")
 @Table(name="clients")
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
-    private Boolean confirm_client;
-    private String attendance_date;
-    private String attendance_hour;
+    private Boolean confirmClient;
+    private String attendanceDate;
+    private String attendanceHour;
 }
--- src/main/java/com/example/login_auth_api/dto/client/RegisterClientDTO.java
+++ src/main/java/com/example/login_auth_api/dto/client/RegisterClientDTO.java
@@ -1,4 +1,4 @@
 package com.example.login_auth_api.dto.client;
 
-public record RegisterClientDTO (String name, String attendance_date, String attendance_hour){
+public record RegisterClientDTO (String name, String attendanceDate, String attendanceHour){
 }
